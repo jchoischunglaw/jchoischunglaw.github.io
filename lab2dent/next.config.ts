9f894b17ddba4ed96ac040fc63a8f50bb451833a
jchoischunglaw <jchoi@schunglaw.com>
2025-09-18 11:23:28 -0700

    Fix GitHub Pages loading issues

--- lab2dent/next.config.ts
+++ lab2dent/next.config.ts
@@ -6,8 +6,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/jchoischunglaw.github.io' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/jchoischunglaw.github.io' : '',
 };
 
 export default nextConfig;
